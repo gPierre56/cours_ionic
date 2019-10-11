68ee1c455a6a53ef93a622c091f0e908b598bd0f
Guillaume Pierre <guillaumepierre56@gmail.com>
2019-10-11 15:41:04 +0200

    Utilisation webcam

---
+++ 04-hellocapacitor/www/app.js
@@ -0,0 +1,42 @@
+let content = document.getElementById('contenu');
+
+let listeArticles = [];
+fetch('https://devfest-nantes-2018-api.cleverapps.io/blog').then(function (reponse) {
+    return reponse.json();
+}).then(function (data) {
+    data.forEach((element) => {
+        listeArticles.push(element);
+    })
+}).then(function () {
+    listeArticles.forEach((element) => {
+        content.innerHTML = content.innerHTML.concat(
+            `<ion-card>
+             <img src = "https://devfest2018.gdgnantes.com${element.image}"/>
+             <ion-card-header>
+             <ion-card-title >${element.title}</ion-card-title>
+             </ion-card-header>
+             <ion-card-content>${element.brief}</ion-card-content>
+            </ion-card>`
+        )
+    });
+});
+
+
+
+async function camera() {
+
+    const options = {
+        resultType: capacitorExports.Plugins.CameraResultType.Uri
+    };
+
+        const image = await capacitorExports.Plugins.Camera.getPhoto(options);
+
+
+
+
+
+}
+
+
+
+
